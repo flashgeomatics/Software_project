11195724d6d69e7c7dfbeb16f27ffe32638e7b8d
rukendilara <49392981+rukendilara@users.noreply.github.com>
2019-06-14 14:39:50 +0200

    Delete bike_table.py

--- BIKOD_FinalVersion/bike_table.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-import pandas as pd
-
-engine = create_engine('postgresql://postgres:ruking29@localhost/se4g')
-df = pd.read_csv('C:/Users/Lenovo/user_server/env/BIKOD_2/bike.csv')
-df.to_sql('bike', engine, if_exists='replace')
